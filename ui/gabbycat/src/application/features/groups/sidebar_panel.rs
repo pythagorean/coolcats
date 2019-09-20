e3f05cfe1f248b2690c57985d183d326e59f8a20
Michael Goldman <mike.goldman@allstreets.org>
2019-09-20 11:32:50 -0400

    Partially implement who to follow and sidebar panels

--- ui/gabbycat/src/application/features/groups/sidebar_panel.rs
+++ ui/gabbycat/src/application/features/groups/sidebar_panel.rs
@@ -1,12 +1,29 @@
 use yew::prelude::*;
 
 use gabbycat_macros::ImplComponent;
+use crate::application::facilities::{formatted_message::FormattedMessage, icon::Icon};
 
 #[derive(ImplComponent)]
 pub struct GroupSidebarPanel;
 
 impl Renderable<GroupSidebarPanel> for GroupSidebarPanel {
     fn view(&self) -> Html<Self> {
-        html! {}
+        html! {
+            <div class = "wtf-panel group-sidebar-panel">
+                <div class = "wtf-panel-header">
+                    <Icon id = "users", class = "wtf-panel-header__icon" />
+                    <span class = "wtf-panel-header__label">
+                        <FormattedMessage id = "groups-sidebar-panel-title", default_message = "Groups You're In" />
+                    </span>
+                </div>
+
+                <div className = "wtf-panel__content">
+                    <div class = "group-sidebar-panel__items">
+                        //{groupIds.slice(0, 10).map(groupId => <Item key={groupId} id={groupId} />)}
+                        //{count > 10 && <Link className="group-sidebar-panel__items__show-all" to='/groups/browse/member'>{intl.formatMessage(messages.show_all)}</Link>}
+                    </div>
+                </div>
+            </div>
+        }
     }
 }
--- ui/gabbycat/src/application/features/ui/who_to_follow_panel.rs
+++ ui/gabbycat/src/application/features/ui/who_to_follow_panel.rs
@@ -1,12 +1,37 @@
 use yew::prelude::*;
 
 use gabbycat_macros::ImplComponent;
+use crate::application::facilities::{formatted_message::FormattedMessage, icon::Icon};
 
 #[derive(ImplComponent)]
 pub struct WhoToFollowPanel;
 
 impl Renderable<WhoToFollowPanel> for WhoToFollowPanel {
     fn view(&self) -> Html<Self> {
-        html! {}
+        html! {
+            <div class = "wtf-panel">
+                <div class = "wtf-panel-header">
+                    <Icon id = "users", class = "wtf-panel-header__icon" />
+                    <span class = "wtf-panel-header__label">
+                        <FormattedMessage id = "who_to_follow-title", default_message = "Who To Follow" />
+                    </span>
+                </div>
+                <div class = "wtf-panel__content">
+                    <div class = "wtf-panel__list">
+                        /*
+                        {suggestions && suggestions.map(accountId => (
+                          <AccountContainer
+                            key={accountId}
+                            id={accountId}
+                            actionIcon='times'
+                            actionTitle={intl.formatMessage(messages.dismissSuggestion)}
+                            onActionClick={dismissSuggestion}
+                          />
+                        ))}
+                        */
+                    </div>
+                </div>
+            </div>
+        }
     }
 }
