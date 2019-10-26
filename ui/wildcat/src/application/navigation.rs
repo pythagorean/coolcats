fc996528db96d5845933b5d34c3eab7d5f10dd5d
Michael Goldman <mike.goldman@allstreets.org>
2019-10-26 06:06:18 -0400

    Add some navigation elements

--- ui/wildcat/src/application/navigation.rs
+++ ui/wildcat/src/application/navigation.rs
@@ -8,7 +8,21 @@ use crate::application::{
     routes::RouterTarget,
 };
 
-use_locale_values!["settings-back"];
+use_locale_values![
+    "settings-back",
+    "settings-profile",
+    "settings-appearance",
+    "settings-featured_tags",
+    "settings-preferences",
+    "settings-relationships",
+    "filters-index-title",
+    "settings-account",
+    "settings-import_and_export",
+    "settings-development",
+    "moderation-title",
+    "admin-title",
+    "auth-logout"
+];
 
 #[derive(LocaleComponent, UsesLocaleValues)]
 pub struct Navigation {
@@ -28,6 +42,81 @@ impl Renderable<Navigation> for Navigation {
                         {fa_icon("chevron-left fw")}
                         {t("settings-back")}
                     </>})}
+                </li>
+                // vvv These are incomplete static elements vvv
+                <li id = "profile", class = "selected">
+                    <a class = "selected", href = "/settings/profile">
+                        {fa_icon("user fw")}
+                        {t("settings-profile")}
+                    </a>
+                    <ul>
+                        <li id = "profile", class = "selected simple-navigation-active-leaf">
+                            <a class = "selected", href = "/settings/profile">
+                                {fa_icon("pencil fw")}
+                                {t("settings-appearance")}
+                            </a>
+                        </li>
+                        <li id = "featured_tags">
+                            <a href = "/settings/featured_tags">
+                                {fa_icon("hashtag fw")}
+                                {t("settings-featured_tags")}
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li id = "preferences">
+                    <a href = "/settings/preferences">
+                        {fa_icon("cog fw")}
+                        {t("settings-preferences")}
+                    </a>
+                </li>
+                <li id = "relationships">
+                    <a href = "/relationships">
+                        {fa_icon("users fw")}
+                        {t("settings-relationships")}
+                    </a>
+                </li>
+                <li id = "filters">
+                    <a href = "/filters">
+                        {fa_icon("filter fw")}
+                        {t("filters-index-title")}
+                    </a>
+                </li>
+                <li id = "security">
+                    <a href = "/auth/edit">
+                        {fa_icon("lock fw")}
+                        {t("settings-account")}
+                    </a>
+                </li>
+                <li id = "data">
+                    <a href = "/settings/export">
+                        {fa_icon("cloud-download fw")}
+                        {t("settings-import_and_export")}
+                    </a>
+                </li>
+                <li id = "development">
+                    <a href = "/settings/applications">
+                        {fa_icon("code fw")}
+                        {t("settings-development")}
+                    </a>
+                </li>
+                <li id = "moderation">
+                    <a href = "/admin/reports">
+                        {fa_icon("gavel fw")}
+                        {t("moderation-title")}
+                    </a>
+                </li>
+                <li id = "admin">
+                    <a href = "/admin/dashboard">
+                        {fa_icon("cogs fw")}
+                        {t("admin-title")}
+                    </a>
+                </li>
+                <li id = "logout">
+                    <a data-method = "delete", href = "/auth/signout">
+                        {fa_icon("sign-out fw")}
+                        {t("auth-logout")}
+                    </a>
                 </li>
             </ul>
         }
--- ui/wildcat/src/application/resources/locales/en.ftl
+++ ui/wildcat/src/application/resources/locales/en.ftl
@@ -1,16 +1,28 @@
 # https://projectfluent.org/fluent/guide/
+admin-title = Administration
 auth-delete_account = Delete account
 auth-delete_account_html = If you wish to delete your account, you can <a href="%{path}">proceed
   here</a>. You will be asked for confirmation.
+auth-logout = Logout
 auth-migrate_account = Move to a different account
 auth-migrate_account_html = If you wish to redirect this account to a different one, you can
   <a href="%{path}">configure it here</a>.
+filters-index-title = Filters
 generic-copy = Copy
 generic-save_changes = Save changes
 migrations-incoming_migrations = Moving from a different account
 migrations-incoming_migrations_html = To move from another account to this one, first you need to
   <a href="%{path}">create an account alias</a>.
+moderation-title = Moderation
+settings-account = Account
+settings-appearance = Appearance
 settings-back = Back to Coolcats
+settings-development = Development
+settings-featured_tags = Featured hashtags
+settings-import_and_export = Import and export
+settings-preferences = Preferences
+settings-profile = Profile
+settings-relationships = Follows and followers
 simple_form-labels-account-fields-name = Label
 simple_form-labels-account-fields-value = Content
 simple_form-hints-defaults-fields = You can have up to 4 items displayed as a table on your profile
